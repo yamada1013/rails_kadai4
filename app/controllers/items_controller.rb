8d413be9cc9770bd0f6c5454664fb1d11574ee81
Yuko Yamada <oly.oly.onlyno1@gmail.com>
2020-09-16 18:41:35 +0900

    rails課題4

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,23 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(name: item_params[:name], price: item_params[:price], user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price)
+  end
+end
